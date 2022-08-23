bd5416b62fd713b70601d34d73a64b9ed004b671
BNMali <111843496+BNMali@users.noreply.github.com>
2022-08-23 12:10:32 +0530

    Add files via upload

---
+++ Locator_Demo/src/main/java/demo/lounch_browser.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class lounch_browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
---
+++ Locator_Demo/src/test/java/Locator/Locator_Demo/AppTest.java
@@ -0,0 +1,13 @@
+package Locator.Locator_Demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
